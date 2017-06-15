65da4425f548ecd4017882767a3071554518002e
Eugene Zarybinskiu <zarybinskiu@gmail.com>
2017-06-15 20:09:01 +0300

    Solve problem with mocking custom scalar types.
    Monkey patching schema for custom scalar types mocking.
    Change config components. (currently dotenv module configure function is invoked once)

--- api/schemas/rootQuery.schema.js
+++ api/schemas/rootQuery.schema.js
@@ -1,11 +1,13 @@
 import AuthorSchema from './author.schema';
 import PostSchema from './post.schema';
+import UserSchema from './user.schema';
 
 const RootQuerySchema = `
   type RootQuery {
     author(id: Int!): Author
     posts: [Post]
+    users: [User!]!
   }
 `;
 
-export default () => [RootQuerySchema, AuthorSchema, PostSchema];
+export default () => [RootQuerySchema, AuthorSchema, PostSchema, UserSchema];
